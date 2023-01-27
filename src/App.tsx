ff6297c59f834d261b511fc15d5df15d4b8770b8
Leonardo Santos <devsleonardosantos@outlook.com.br>
2023-01-27 12:30:46 -0300

    Pagina de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+// Estilo
 import './App.css'
+
+// Pages
+import Login from './Pages/Login/Index'
+import DashBoard from './Pages/DashBoard/Index'
+
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Login/>
+      {/* <DashBoard/> */}
     </div>
   )
 }
